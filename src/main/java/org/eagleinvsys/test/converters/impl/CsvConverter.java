42f93a90d637ff993b35f3b498322eebcab9c5ab
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-16 00:40:36 +0300

    Implement methods convert

--- src/main/java/org/eagleinvsys/test/converters/impl/CsvConverter.java
+++ src/main/java/org/eagleinvsys/test/converters/impl/CsvConverter.java
@@ -2,8 +2,14 @@ package org.eagleinvsys.test.converters.impl;
 
 import org.eagleinvsys.test.converters.Converter;
 import org.eagleinvsys.test.converters.ConvertibleCollection;
+import org.eagleinvsys.test.converters.ConvertibleMessage;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Iterator;
+
+import static org.eagleinvsys.test.converters.constants.MyConstants.*;
+import static org.eagleinvsys.test.converters.utils.MyUtils.escapeSpecialCharacters;
 
 public class CsvConverter implements Converter {
 
@@ -14,8 +20,39 @@ public class CsvConverter implements Converter {
      * @param outputStream        output stream to write CSV conversion result as text to
      */
     @Override
-    public void convert(ConvertibleCollection collectionToConvert, OutputStream outputStream) {
-        // TODO: implement
-    }
+    public void convert(ConvertibleCollection collectionToConvert, OutputStream outputStream) throws IOException {
+        if (collectionToConvert == null || outputStream == null || collectionToConvert.getHeaders().isEmpty()) {
+            throw new NullPointerException();
+        }
 
+        String header = null;
+        for (Iterator<String> ic = collectionToConvert.getHeaders().iterator();
+             ic.hasNext(); ) {
+            header = ic.next();
+            outputStream.write(
+                    (
+                            escapeSpecialCharacters(header != null ? header : "", SEP) + (ic.hasNext() ? SEP : "")
+                    ).getBytes());
+
+        }
+        if (!(collectionToConvert.getHeaders().size() == 1 && header == null)) {
+            outputStream.write(END.getBytes());
+        }
+
+        for (Iterator<ConvertibleMessage> icm = collectionToConvert.getRecords().iterator();
+             icm.hasNext(); ) {
+            ConvertibleMessage cm = icm.next();
+            for (Iterator<String> icc = collectionToConvert.getHeaders().iterator();
+                 icc.hasNext(); ) {
+                String curVal = cm.getElement(icc.next());
+                outputStream.write(
+                        (
+                                escapeSpecialCharacters(curVal != null ? curVal : "", SEP)
+                                        + (icc.hasNext() ? SEP : "")
+                        ).getBytes()
+                );
+            }
+            if (icm.hasNext()) outputStream.write(END.getBytes());
+        }
+    }
 }
--- src/main/java/org/eagleinvsys/test/converters/impl/StandardCsvConverter.java
+++ src/main/java/org/eagleinvsys/test/converters/impl/StandardCsvConverter.java
@@ -1,10 +1,13 @@
 package org.eagleinvsys.test.converters.impl;
 
+import org.eagleinvsys.test.converters.ConvertibleMessage;
 import org.eagleinvsys.test.converters.StandardConverter;
 
+import java.io.IOException;
+import java.io.NotSerializableException;
 import java.io.OutputStream;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class StandardCsvConverter implements StandardConverter {
 
@@ -21,8 +24,26 @@ public class StandardCsvConverter implements StandardConverter {
      * @param outputStream        output stream to write CSV conversion result as text to
      */
     @Override
-    public void convert(List<Map<String, String>> collectionToConvert, OutputStream outputStream) {
-        // TODO: implement by using csvConverter
+    public void convert(List<Map<String, String>> collectionToConvert, OutputStream outputStream) throws IOException {
+        if (collectionToConvert == null || outputStream == null || collectionToConvert.isEmpty()) {
+            throw new NullPointerException();
+        }
+        Map<String, String> firstMap = collectionToConvert.get(0);
+
+        if (firstMap == null) {
+            throw new NullPointerException();
+        }
+        Set<String> setKeys = firstMap.keySet();
+        //TODO: to need to optimize
+        for (Set<String> set : collectionToConvert.stream().map(Map::keySet).collect(Collectors.toSet())) {
+            if (!set.equals(setKeys)) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        ConvertibleCollectionImpl conCol = new ConvertibleCollectionImpl(collectionToConvert);
+
+        csvConverter.convert(conCol, outputStream);
     }
 
 }
